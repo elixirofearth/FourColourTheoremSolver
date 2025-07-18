bc0d3a578b3efe27682ced3f1b86859df5be1cfb
Andy Tran <anhquoctran006@gmail.com>
2025-07-18 14:04:28 -0600

    Revert "increase buffer size"
    This reverts commit c45c7ca879767c5f660d3ce8f6f7edb6d842c5bf.

--- server/api-gateway-service/src/main/java/com/fourcolour/gateway/config/WebConfig.java
+++ server/api-gateway-service/src/main/java/com/fourcolour/gateway/config/WebConfig.java
@@ -2,47 +2,17 @@ package com.fourcolour.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.core5.util.Timeout;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
-        // Create connection pool manager with custom settings
-        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
-
-        // Create request configuration with timeouts
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectionRequestTimeout(Timeout.ofSeconds(30))
-                .setResponseTimeout(Timeout.ofMinutes(5))
-                .build();
-
-        // Create HTTP client with custom settings for large responses
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .setDefaultRequestConfig(requestConfig)
-                .build();
-
-        // Create request factory with the configured HTTP client
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        
-        // Set connection timeout
-        requestFactory.setConnectTimeout(30000); // 30 seconds
-
-        return new RestTemplate(requestFactory);
+        return new RestTemplate();
     }
 
     @Bean
