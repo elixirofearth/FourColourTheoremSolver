a641b7c7fd53408cc330c05b3aa28cb091cd1457
Andy Tran <anhquoctran006@gmail.com>
2025-07-22 13:38:32 -0600

    Update AuthenticationIntegrationTest.java

--- server/authentication-service/src/test/java/com/fourcolour/auth/integration/AuthenticationIntegrationTest.java
+++ server/authentication-service/src/test/java/com/fourcolour/auth/integration/AuthenticationIntegrationTest.java
@@ -22,8 +22,6 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.core5.util.Timeout;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
