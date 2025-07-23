c43a9f335aeac5ebb0eca000e2fbd6bc5fa466a8
Andy Tran <anhquoctran006@gmail.com>
2025-07-23 04:47:36 -0600

    Update playwright.config.ts

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -5,13 +5,13 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests/e2e",
-  /* Run tests in files in parallel */
+  /* Run tests in files in series */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  /* Fail the build on CI if you accidentally left test.only in the source code */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  /* Opt out of parallel tests */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
